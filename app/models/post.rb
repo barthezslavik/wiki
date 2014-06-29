dc23d8736ffbe2de23f606ea8b9e9534adb05e01
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2014-06-29 15:30:00 +0300

    fetch properties

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,13 @@
 class Post < ActiveRecord::Base
   def morphology
-    content
+    base = content.split
+    objects = []
+    properties = []
+    base.each do |b|
+      next if %w(с в не бы и или все).include? b
+      objects << b
+      properties << b if %w(ое ой).any? { |x| b.ends_with?(x) }
+    end
+    { base: base, properties: properties }
   end
 end
